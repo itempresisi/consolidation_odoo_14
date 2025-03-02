4c2b3d4e70c1f02c1a041b3d75f7349fe88c5e36
itempresisi <it.empresisi@gmail.com>
2025-03-02 11:03:35 +0700

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import consolidation_group
-from . import consolidation_period
-from . import consolidation_account
-from . import consolidation_journal
-from . import intercompany
-from . import analysis
-from . import res_company
